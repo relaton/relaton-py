82c0e33590e843d3529d29e834a11eda232b975b
Anton Strogonoff <anton@strogonoff.name>
2022-04-30 12:20:45 +0700

    chore: re-import data models from `relaton.models`
    for convenience.

--- relaton/models/__init__.py
+++ relaton/models/__init__.py
@@ -20,3 +20,33 @@ They are mixed for a few reasons:
                To obtain a full dictionary,
                use :func:`common.pydantic.unpack_dataclasses` utility.
 """
+
+from .models.bibdata import *
+from .models.copyrights import *
+from .models.people import *
+from .models.orgs import *
+from .models.contacts import *
+from .models.links import *
+from .models.dates import *
+from .models.strings import *
+
+
+__all__ = (
+  'BibliographicItem',
+  'DocID',
+  'Series',
+  'BiblioNote',
+  'Contributor',
+  'Relation',
+  'Person',
+  'PersonName',
+  'PersonAffiliation',
+  'Organization',
+  'Contact',
+  'Date',
+  'Link',
+  'Title',
+  'GenericStringValue',
+  'FormattedContent',
+)
+
