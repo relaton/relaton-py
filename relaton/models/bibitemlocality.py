bc0cb71c7cffd051c844478f8940c0e684fa9e52
stefanomunarini <ssteee@gmail.com>
2022-06-03 10:46:15 +0100

    chore: improve docs

--- relaton/models/bibitemlocality.py
+++ relaton/models/bibitemlocality.py
@@ -7,7 +7,18 @@ from relaton.models import GenericStringValue
 
 @dataclass
 class BibItemLocality:
-    """Bibliographic item locality."""
+    """
+    The extent or location of a bibliographic item being referred to.
+
+    A sequence of locality elements is meant to indicate hierarchical ordering,
+    from greater to smaller.
+
+    [example]
+    Chapter, then page, then paragraph.
+
+    A discontinuous range can be represented by using two adjacent localities
+    with the same type.
+    """
 
     type: str
     reference_from: GenericStringValue
