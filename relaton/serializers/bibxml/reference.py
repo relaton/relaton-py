4fc0c2739cb074aa535e9f1b0958a97e843c8ff7
stefanomunarini <ssteee@gmail.com>
2022-06-07 17:05:52 +0100

    fix: remove serializer

--- relaton/serializers/bibxml/reference.py
+++ relaton/serializers/bibxml/reference.py
@@ -121,24 +121,6 @@ def create_reference(item: BibliographicItem) -> Element:
     except ValueError:
         pass
     else:
-        ref.set('anchor', anchor)
-
-    # BibItemLocality
-    extent: LocalityStack = item.extent or []
-    info = []
-    name = ""
-    for locality in extent.locality:
-        if locality.type == "container-title":
-            name = locality.reference_from
-        if locality.type == "volume":
-            info.append("vol. %s" % locality.reference_from)
-        elif locality.type == "issue":
-            info.append("no. %s" % locality.reference_from)
-        elif locality.type == "page":
-            info.append("pp. %s" % locality.reference_from)
-    ref.append(E.seriesInfo(
-        name=name,
-        value=", ".join(info),
-    ))
+        ref.set("anchor", anchor)
 
     return ref
