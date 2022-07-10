7a64faa5414201bcc55c876b978a13b99859c8b5
Anton Strogonoff <anton@strogonoff.name>
2022-07-10 23:54:20 +0700

    fix(bibxml): ensure anchor is valid xs:id string
    BREAKING: Doctype-specific Internet Drafts anchor formatting
    is no longer done. Callers can do that themselves.

--- relaton/serializers/bibxml/anchor.py
+++ relaton/serializers/bibxml/anchor.py
@@ -1,27 +1,17 @@
 from typing import List
+from string import ascii_letters, digits
+import re
 
 from ...models.bibdata import BibliographicItem, DocID
 from ...util import as_list
 
+
 __all__ = (
     'get_suitable_anchor',
-    'format_internet_draft_anchor',
+    'to_valid_xsid',
+    'XSID_REGEX',
+    'XSID_ILLEGAL',
 )
-
-
-def format_internet_draft_anchor(id: str, versioned: bool = False) -> str:
-    """
-    Returns an anchor specific for an Internet Draft.
-    If ``versioned`` is True, ``id`` is assumed to contain a version.
-
-    :param str id: primary identifier for the I-D
-    :param bool versioned: whether ``id`` is for I-D version or I-D as a whole
-    :rtype: str
-    """
-    if versioned:
-        return id
-    else:
-        return f"I-D.{id}"
 
 
 def get_suitable_anchor(item: BibliographicItem) -> str:
@@ -31,29 +21,50 @@ def get_suitable_anchor(item: BibliographicItem) -> str:
     :param item: a :class:`bib_models.bibdata.BibliographicItem` instance
     :returns str: a string to be used as anchor
     :rtype: str
-    :raises ValueError: unable to obtain an anchor
+    :raises ValueError: unable to obtain an anchor, e.g. item has no docids
     """
 
     docids: List[DocID] = as_list(item.docid or [])
 
-    for docid in docids:
-        if docid.type.lower() == 'internet-draft':
-            is_versioned_draft = item.version and any([
-                (v and v.draft)
-                for v in as_list(item.version)
-                # mypy error here ^ suggests it doesn’t handle generics well
-            ])
-            return format_internet_draft_anchor(
-                docid.id,
-                versioned=True if is_versioned_draft else False)
-        elif docid.scope == 'anchor':
-            return docid.id
-
     try:
         anchor_docid: DocID = (
+            # Prefer primary
             [d for d in docids if d.primary]
+            # Fallback case (docid.scope may be going away)
+            or [d for d in docids if getattr(d, 'scope', '') == 'anchor']
+            # Otherwise, take any docid
             or docids)[0]
     except IndexError:
         raise ValueError("No suitable anchor could be determined")
     else:
-        return anchor_docid.id
+        if XSID_REGEX.match(anchor_docid.id) is not None:
+            return anchor_docid.id
+        else:
+            return to_valid_xsid(anchor_docid.id)
+
+
+def to_valid_xsid(val: str) -> str:
+    """
+    Transforms a string into a valid xs:id value.
+    Transformation is lossy and irreversible.
+    """
+    return XSID_ILLEGAL.sub('', re.sub(
+        r'^\d',
+        r'_\g<0>',
+        re.sub(
+            r'[-\s]+',
+            '_',
+            val
+            .replace('/', '-')
+            .replace(':', '.')
+            .strip('-_')
+        )
+    ))
+
+
+XSID_REGEX = re.compile(r'^[a-zA-Z_][-.\w]*$')
+"""A regular expression matching a full valid xs:id values."""
+
+XSID_ILLEGAL = re.compile(r'[^-.\w]')
+"""A regular expression matching xs:id characters that are invalid
+anywhere within an xs:id string."""
