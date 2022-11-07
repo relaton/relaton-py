696b581cbae70d9754ff3aae201039b0b67a6315
Anton <anton@strogonoff.name>
2022-11-07 13:45:14 +0700

    chore: expand on Copyright alias comment

--- relaton/models/copyrights.py
+++ relaton/models/copyrights.py
@@ -9,6 +9,8 @@ from .orgs import Organization
 from .people import Person
 
 
+# ``from`` is reserved in Python, so if we can’t alias it we’ll just use
+# a ``TypedDict`` instead of a dataclass.
 # Pydantic dataclasses don’t actually support aliases, contrary to docs
 Copyright = TypedDict('Copyright', {
     'from': int,
