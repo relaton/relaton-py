42cb2b426650adaf5074aa6a9729528dba90c75e
stefanomunarini <ssteee@gmail.com>
2022-11-13 17:18:04 +0000

    chore: make directory for each dataset

--- relaton/tests/validate_data_against_models.py
+++ relaton/tests/validate_data_against_models.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 import yaml
 from lxml import etree
+from pydantic import ValidationError
 
 from relaton.models import BibliographicItem
 from relaton.serializers.bibxml import serialize
@@ -17,38 +18,40 @@ class DataValidationTestCase(TestCase):
         file_path = os.path.join(module_dir, "static/schemas/v3.xsd")
         self.xmlschema = etree.XMLSchema(file=file_path)
 
-    def _validate_yaml_data(self, filename):
+    def _validate_yaml_data(self, dataset, filename):
         module_dir = os.path.dirname(__file__)
-        file_path = os.path.join(module_dir, f"static/fixtures/v{self.schema_version}/{filename}.yaml")
+        file_path = os.path.join(module_dir, f"static/fixtures/v{self.schema_version}/{dataset}/{filename}.yaml")
         with open(file_path) as stream:
             yaml_object = yaml.safe_load(stream)
             bibitem = BibliographicItem(**yaml_object)
             serialized_data = serialize(bibitem)
             self.xmlschema.assertValid(serialized_data)
 
+    # TODO: add multiple documents for each dataset
+
     def test_validate_rfcs_data(self):
-        self._validate_yaml_data("RFC0001")
+        self._validate_yaml_data("rfcs", "RFC0001")
 
     def test_validate_misc_data(self):
-        self._validate_yaml_data("reference.ANSI.T1-102.1987")
+        self._validate_yaml_data("misc", "reference.ANSI.T1-102.1987")
 
     def test_validate_internet_drafts_data(self):
-        self._validate_yaml_data("draft--pale-email-00")
+        self._validate_yaml_data("internet_drafts", "draft--pale-email-00")
 
     def test_validate_w3c_data(self):
-        self._validate_yaml_data("2dcontext")
+        self._validate_yaml_data("w3c", "2dcontext")
 
     def test_validate_threegpp_data(self):
-        self._validate_yaml_data("TR_00.01U_UMTS_3.0.0")
+        self._validate_yaml_data("3gpp", "TR_00.01U_UMTS_3.0.0")
 
     def test_validate_ieee_data(self):
-        self._validate_yaml_data("AIEE_11-1943")
+        self._validate_yaml_data("ieee", "AIEE_11-1943")
 
     def test_validate_iana_data(self):
-        self._validate_yaml_data("_6lowpan-parameters")
+        self._validate_yaml_data("iana", "_6lowpan-parameters")
 
     def test_validate_rfcsubseries_data(self):
-        self._validate_yaml_data("BCP0003")
+        self._validate_yaml_data("rfcsubseries", "BCP0003")
 
     def test_validate_nist_data(self):
-        self._validate_yaml_data("NBS_BH_1")
+        self._validate_yaml_data("nist", "NBS_BH_1")
