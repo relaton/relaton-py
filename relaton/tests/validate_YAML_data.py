cf007cec781fe93d88c6749a7a9bedf9bc8fe31c
stefanomunarini <ssteee@gmail.com>
2022-11-14 18:16:40 +0000

    chore: rename file

--- relaton/tests/validate_YAML_data.py
+++ relaton/tests/validate_YAML_data.py
@@ -11,7 +11,8 @@ from relaton.serializers.bibxml import serialize
 
 class DataValidationTestCase(TestCase):
     """
-    Asserts that XML output conforms to the authoritative schema.
+    Validate YAML data against model format version and
+    validate XML output against authoritative schema.
     """
     schema_version = "1.13.0"
 
@@ -20,41 +21,41 @@ class DataValidationTestCase(TestCase):
         file_path = os.path.join(self.module_dir, "static/schemas/v3.xsd")
         self.xmlschema = etree.XMLSchema(file=file_path)
 
-    def _validate_yaml_data(self, dataset):
+    def _validate_yaml_data_and_xmlschema(self, dataset):
         dataset_dir = os.path.join(self.module_dir, f"static/fixtures/v{self.schema_version}/{dataset}")
         for file in os.listdir(dataset_dir):
             file_path = os.path.join(f"{dataset_dir}/{file}")
             with open(file_path) as stream:
                 yaml_object = yaml.safe_load(stream)
+                # validate YAML data against model format version
                 bibitem = BibliographicItem(**yaml_object)
                 serialized_data = serialize(bibitem)
+                # validate XML output against authoritative schema
                 self.xmlschema.assertValid(serialized_data)
 
-    # TODO: add multiple documents for each dataset
-
     def test_validate_rfcs_data(self):
-        self._validate_yaml_data("rfcs")
+        self._validate_yaml_data_and_xmlschema("rfcs")
 
     def test_validate_misc_data(self):
-        self._validate_yaml_data("misc")
+        self._validate_yaml_data_and_xmlschema("misc")
 
     def test_validate_internet_drafts_data(self):
-        self._validate_yaml_data("internet_drafts")
+        self._validate_yaml_data_and_xmlschema("internet_drafts")
 
     def test_validate_w3c_data(self):
-        self._validate_yaml_data("w3c")
+        self._validate_yaml_data_and_xmlschema("w3c")
 
     def test_validate_threegpp_data(self):
-        self._validate_yaml_data("3gpp")
+        self._validate_yaml_data_and_xmlschema("3gpp")
 
     def test_validate_ieee_data(self):
-        self._validate_yaml_data("ieee")
+        self._validate_yaml_data_and_xmlschema("ieee")
 
     def test_validate_iana_data(self):
-        self._validate_yaml_data("iana")
+        self._validate_yaml_data_and_xmlschema("iana")
 
     def test_validate_rfcsubseries_data(self):
-        self._validate_yaml_data("rfcsubseries")
+        self._validate_yaml_data_and_xmlschema("rfcsubseries")
 
     def test_validate_nist_data(self):
-        self._validate_yaml_data("nist")
+        self._validate_yaml_data_and_xmlschema("nist")
